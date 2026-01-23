ee510f7cfe12234d4bf4ffcfdb3accb1a89747bd
cyhhoo <cyhhoo@gmail.com>
2026-01-23 17:47:34 +0900

    260123 수업내용

---
+++ 15_Choiyeonhoo/chap01_javascript-core-lecture-source/04_function/02_function-call/02_return.js
@@ -0,0 +1,15 @@
+/* 반환문 */
+function hello(name){
+
+    //return
+    //- 현재 함수를 바로 종료 시키고
+    //  해당 함수를 호출한 곳으로 값을 들고 돌아감
+    return `${name}님 안녕하세요.`;
+
+    console.log("test");
+}
+
+console.log(hello("홍길동"));
+
+
+
